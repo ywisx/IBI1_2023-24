aaf8b09e4ba676312ea44bb173e67ab072460c75
yuansiqis <siqi1.23@intl.zju.edu.cn>
2024-03-05 15:59:11 +0800

    4

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -4,8 +4,9 @@ c=30
 d=a-b
 e=b-c
 print(d>=e)
-# Using a combination	of running and strength exercises had a	greater	effect	on	the	5	km	time.	
+# False : Using a combination	of running and strength exercises had a	greater	effect	on	the	5	km	time.	
 X=True
 Y=False
-W="either X or Y"
+W= X or Y
+print(W)
 # W is True
