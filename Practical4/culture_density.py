b24a94d5def0b841f755f94674ac2d1306bec262
yuansiqis <siqi1.23@intl.zju.edu.cn>
2024-03-05 16:30:18 +0800

    5

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -1,1 +1,12 @@
+#Suppose you can leave the lab for i days
+i=0
+a=5
+#Plug in the reproduction rules given.
+while(a<=90):
+    a=a*2
+    i=i+1
+#So we can print out the number of days away from the lab
+print(i-1)
+#We know from the results that we can leave the lab for four days. 
+#You must return at day6, which is the fifth day after leaving for four days.
 
