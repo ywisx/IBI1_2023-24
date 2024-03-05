71b4382b9e6e6c42ad9556f54d1b4b73b8b8e566
yuansiqis <siqi1.23@intl.zju.edu.cn>
2024-03-05 15:15:34 +0800

    3

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -3,7 +3,9 @@ b=36
 c=30
 d=a-b
 e=b-c
-if e<=d :
- print("FALSE")
-else:
- print("TRUE")
+print(d>=e)
+# Using a combination	of running and strength exercises had a	greater	effect	on	the	5	km	time.	
+X=True
+Y=False
+W="either X or Y"
+# W is True
