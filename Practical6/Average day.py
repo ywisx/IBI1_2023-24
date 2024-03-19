d9ef72b701fe8402b28acb8bf5ea4db04df59678
yuansiqis <siqi1.23@intl.zju.edu.cn>
2024-03-19 16:58:25 +0800

    3/19

--- Practical6/Average day.py
+++ Practical6/Average day.py
@@ -1,1 +1,10 @@
+import matplotlib.pyplot as plt  
+Activity=["Sleeping","Classes","Studying","TV","Music","Other"]
+Time_spent=[8,6,3.5,2,1,3.5]
 
+plt.figure()
+plt.pie(Time_spent,labels=Activity,startangle=90)
+plt.title("The average day of a university student")
+plt.show()
+
+plt.clf()
--- Practical6/City Size.py
+++ Practical6/City Size.py
@@ -1,1 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+UK_City=["Edinburgh","Glasgow","Stirling","London"]
+China_City=["Haining","Hangzhou","Shanghai","Beijing"]
+UK_Population=[0.56,0.62,0.04,9.7]
+China_Population=[0.58,8.4,29.9,22.2]
+
+UK_Sort=sorted(UK_Population)
+China_Sort=sorted(China_Population)
+print("Sorted UK City Populations (millions):", UK_Sort)  
+print("Sorted China City Populations (millions):", China_Sort)
+
+plt.figure()
+plt.bar(UK_City,UK_Population)
+plt.title("UK City Sizes")
+plt.xlabel("Cities")
+plt.ylabel("Population(millions)")
+
+plt.show()
+
+plt.clf()
